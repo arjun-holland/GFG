41c8a603a8d9e5b715f47766d9a957a1229a282a
Mallikarjuna <spiderman05116@gmail.com>
2025-03-18 19:03:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Partition Equal Subset Sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    bool solve(vector<int>& arr, int sum,int i,vector<vector<int>>& dp){
+        if(sum == 0)return true;
+        if(i == arr.size())return false;
+        
+        if(dp[i][sum] != -1)return dp[i][sum];
+        bool pick = false, notpick = false;
+        if(arr[i] <= sum) pick = solve(arr,sum-arr[i],i+1,dp);
+        notpick = solve(arr,sum,i+1,dp);
+        
+        return dp[i][sum] =  pick || notpick;
+    }
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(sum+1, -1));
+        return solve(arr, sum, 0, dp);
+    }
+    
+    bool equalPartition(vector<int>& arr) {
+        int total = 0;
+        for(int e : arr)total += e;
+        if(total % 2 != 0)return false;
+        return isSubsetSum(arr,total/2);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        if (ob.equalPartition(arr))
+            cout << "true\n";
+        else
+            cout << "false\n";
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
