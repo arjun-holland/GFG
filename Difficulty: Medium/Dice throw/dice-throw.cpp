fd9c06ea5cc84b6035d02e7a10740b0c0278c1d9
Mallikarjuna <spiderman05116@gmail.com>
2025-05-23 20:40:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Dice throw/dice-throw.cpp
@@ -0,0 +1,21 @@
+//C++ CODE
+class Solution {
+  public: 
+    int solve(int m, int n, int x,int sum,vector<vector<int>>&dp){
+        //base case 
+        if(n==0) return (sum==x)?1:0;
+        //recursive case;
+        if(dp[n][sum]!=-1) return dp[n][sum];
+        int ways=0;
+        for(int i=1;i<=m;i++){
+            if(sum+i<=x){
+                ways+=solve(m,n-1,x,sum+i,dp);
+            }
+        }
+        return dp[n][sum]= ways;
+    }
+    int noOfWays(int m, int n, int x) {
+        vector<vector<int>>dp(n+1,vector<int>(x+1,-1));
+        return solve(m,n,x,0,dp);
+    }
+};
