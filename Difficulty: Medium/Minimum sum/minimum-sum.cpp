a281dd9cc95cc7b12d5bb32b0fc87924483adc54
Mallikarjuna <spiderman05116@gmail.com>
2025-06-23 22:02:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum sum/minimum-sum.cpp
+++ Difficulty: Medium/Minimum sum/minimum-sum.cpp
@@ -1,83 +1,30 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
-
 class Solution {
   public:
-  string minSum(vector<int> &arr) {
-        
-    string n1 = "", n2 = "";
-    sort(arr.begin(), arr.end());
-
-    // Distributing digits into n1 and n2
-    for (int i = 0; i < arr.size(); i++) {
-        if (i % 2 == 0) {
-            n1 += to_string(arr[i]);
-        } else {
-            n2 += to_string(arr[i]);
+    string minSum(vector<int> &arr) {
+        // CodeGenius
+        string n1,n2;
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<arr.size();i++){
+            if(i%2==0) n1+=to_string(arr[i]);
+            else n2+=to_string(arr[i]);
         }
-    }
-
-    // Reverse to get correct order
-    reverse(n1.begin(), n1.end());
-    reverse(n2.begin(), n2.end());
-
-    // Adding n1 and n2
-    int i = 0, j = 0, carry = 0;
-    string ans = "";
-
-    while (i < n1.length() || j < n2.length() || carry) {
-        int sum = carry;
-        
-        if (i < n1.length()) {
-            sum += n1[i++] - '0';//string:char to int
+        reverse(n1.begin(),n1.end());
+        reverse(n2.begin(),n2.end());
+        string ans;
+        int i=0,j=0;
+        int carry=0;
+        while(i<n1.size() || j<n2.size()){
+            int sum=0;
+            if(i<n1.size())sum+=n1[i++]-'0';
+            if(j<n2.size())sum+=n2[j++]-'0';
+            sum+=carry;
+            carry=sum/10;
+            sum%=10;
+            ans+=to_string(sum);
         }
-        if (j < n2.length()) {
-            sum += n2[j++] - '0';
+        ans+=to_string(carry);
+        while(ans.back()=='0')ans.pop_back();
+        reverse(ans.begin(),ans.end());
+        return ans;
         }
-
-        carry = sum / 10;
-        ans += to_string(sum % 10);
-    }
-
-    // Reverse result to get final answer
-    reverse(ans.begin(), ans.end());
-    
-    // Remove leading zeros if any
-    while (ans.size() > 1 && ans[0] == '0') {
-        ans.erase(ans.begin());
-    }
-
-    return ans;
-    }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> a;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            a.push_back(number);
-        }
-
-        Solution ob;
-        string ans = ob.minSum(a);
-        cout << ans << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
